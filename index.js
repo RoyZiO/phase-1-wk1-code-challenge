abb5e4056184b2feff4bf011b743b02b2455537f
RoyZiO <nziokinroy@gmail.com>
2022-11-28 08:58:18 +0300

    Added code comments

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function studentGrade(totMarks) {               //function for generating studen
             return "A";
         }
         else {
-        if (totMarks >= 60 && totMarks <= 79) {
+        if (totMarks >= 60 && totMarks <= 79) {             //if...else conditional staements to determine letter grade based on the number of marks scored//
         return "B";
-        }
+        }   
             else {
             if (totMarks >= 50 && totMarks <= 59) {
             return "C";
@@ -28,7 +28,7 @@ function studentGrade(totMarks) {               //function for generating studen
             }
         }
     }
-    return studentGrade(totMarks); 
+    return studentGrade(totMarks);              //the function returns the letter grade depending on which range a particular score falls.
 }
 console.log(studentGrade(90));
 
@@ -38,14 +38,14 @@ function speedDetector(carSpeed) {
     const speedLimit = 70
     const speedRate = 5
 
-    if(carSpeed <= speedLimit) {
+    if(carSpeed <= speedLimit) {            //conditional statement to return 'OK if the speed of a vehicle is less than or equal to the speed limit//
         return "OK";
     }
 
-    const excessSpeed = carSpeed - speedLimit;
+    const excessSpeed = carSpeed - speedLimit;          //excessSpeed is used to represent the degree at which a driver has exceeded the speed limit//
 
     let  demeritPoints = 0;
-    for(let i = 0; i < excessSpeed; i+= speedRate) {
+    for(let i = 0; i < excessSpeed; i+= speedRate) {   //for loop to define the point system for awarding demerit points based on the degree of overspeeding//
 
         demeritPoints += 1
     
